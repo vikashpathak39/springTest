fb9c274e892460cd2f5003109fcd49baf6463038
vikashpathak39 <vikashpathak39@gmail.com>
2019-08-26 07:53:37 +0530

    uploading file

--- src/main/java/com/org/demobank/App.java
+++ src/main/java/com/org/demobank/App.java
@@ -1,6 +1,6 @@
 package com.org.demobank;
 
-
+//
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
